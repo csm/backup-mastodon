a1ffc4d6416b4dd2663487412a2ecbad1ae32c8a
Casey Marshall <casey@metastatic.org>
2022-11-18 13:26:19 -0800

    Add cleanup-mastodon support.

---
+++ cleanup-mastodon.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export RAILS_ENV=production
+sudo --preserve-env=list=RAILS_ENV -u mastodon /home/mastodon/live/bin/tootctl media remove --days=14
+sudo --preserve-env=list=RAILS_ENV -u mastodon /home/mastodon/live/bin/tootctl preview_cards remove --days=14
